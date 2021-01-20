474b7d16a49cbf1ae263c1ed3280e7ffe7beb24b
Jue Liu <vickyjue.liu@mail.utoronto.ca>
2021-01-20 18:40:13 -0500

    add KVMessageImpl

--- M1/m1/src/shared/messages/KVMessageImpl.java
+++ M1/m1/src/shared/messages/KVMessageImpl.java
@@ -1,18 +1,29 @@
 package shared.messages;
 
 public class KVMessageImpl implements KVMessage {
+
+    private String key;
+    private String value;
+    private StatusType status;
+
+    public KVMessageImpl(String key, String value, StatusType status) {
+        this.key = key;
+        this.value = value;
+        this.status = status;
+    }
+
     @Override
     public String getKey() {
-        return null;
+        return key;
     }
 
     @Override
     public String getValue() {
-        return null;
+        return value;
     }
 
     @Override
     public StatusType getStatus() {
-        return null;
+        return status;
     }
 }
