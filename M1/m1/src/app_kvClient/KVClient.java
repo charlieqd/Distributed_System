3e3dc23581c191d6d93a4ad028b6c655301d90bc
TommyX12 <tommyx058@gmail.com>
2021-01-20 15:59:19 -0500

    Initial setup

---
+++ M1/m1/src/app_kvClient/KVClient.java
@@ -0,0 +1,20 @@
+package app_kvClient;
+
+import client.KVCommInterface;
+
+public class KVClient implements IKVClient {
+    @Override
+    public void newConnection(String hostname, int port) throws Exception {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public KVCommInterface getStore() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Client works!");
+    }
+}
