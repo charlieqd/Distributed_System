75f46d8830bb6923de6cfb707a289b5a3083cf5c
TommyX12 <tommyx058@gmail.com>
2021-01-21 19:26:36 -0500

    Improved string escape, added csv string split stub

--- M1/m1/src/shared/Util.java
+++ M1/m1/src/shared/Util.java
@@ -14,10 +14,18 @@ public class Util {
         int length = s.length();
         for (int i = 0; i < length; ++i) {
             char c = s.charAt(i);
-            if (c == ',' || c == '\\' || c == '\n') {
+            if (c == ',' || c == '\\') {
                 sb.append('\\');
+                sb.append(c);
+            } else if (c == '\n') {
+                sb.append('\\');
+                sb.append('n');
+            } else if (c == '\r') {
+                sb.append('\\');
+                sb.append('r');
+            } else {
+                sb.append(c);
             }
-            sb.append(c);
         }
         return sb.toString();
     }
@@ -35,10 +43,35 @@ public class Util {
         for (int i = 0; i < length; ++i) {
             char c = s.charAt(i);
             if (c == '\\') {
-                continue;
+                if (i + 1 >= length) {
+                    // At the end of the string
+                    break;
+                }
+                char next = s.charAt(i + 1);
+                if (next == 'n') {
+                    sb.append('\n');
+                } else if (next == 'r') {
+                    sb.append('\r');
+                } else {
+                    sb.append(next);
+                }
+                ++i; // Skip over the next character
+            } else {
+                sb.append(c);
             }
-            sb.append(c);
         }
         return sb.toString();
+    }
+
+    /**
+     * Given a line (without line ending), split the line by comma into an array
+     * of strings. Each column is assumed to be the output of {@link
+     * #escapeCSVString}, and will be unescaped before being returned.
+     *
+     * @return the list of columns.
+     */
+    public static String[] csvSplitLine(String line) {
+        // TODO implement
+        return new String[0];
     }
 }
