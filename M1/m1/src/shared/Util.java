f942618e6f01393617c364a06ba5215d18f8c86c
TommyX12 <tommyx058@gmail.com>
2021-01-21 19:15:01 -0500

    Implemented string escape

--- M1/m1/src/shared/Util.java
+++ M1/m1/src/shared/Util.java
@@ -10,19 +10,35 @@ public class Util {
      * @return the escaped string.
      */
     public static String escapeCSVString(String s) {
-        // TODO implement
-        return s;
+        StringBuilder sb = new StringBuilder();
+        int length = s.length();
+        for (int i = 0; i < length; ++i) {
+            char c = s.charAt(i);
+            if (c == ',' || c == '\\' || c == '\n') {
+                sb.append('\\');
+            }
+            sb.append(c);
+        }
+        return sb.toString();
     }
 
     /**
-     * Decode a string encded with {@link #escapeCSVString} back into its
+     * Decode a string encoded with {@link #escapeCSVString} back into its
      * original form.
      *
      * @param s the escaped string.
      * @return the original string.
      */
     public static String unescapeCSVString(String s) {
-        // TODO implement
-        return s;
+        StringBuilder sb = new StringBuilder();
+        int length = s.length();
+        for (int i = 0; i < length; ++i) {
+            char c = s.charAt(i);
+            if (c == '\\') {
+                continue;
+            }
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
