d79f0c6e814a30d1814a2b539fa8cd736566aa36
TommyX12 <tommyx058@gmail.com>
2021-01-21 19:08:24 -0500

    Added stub for string escape

---
+++ M1/m1/src/shared/Util.java
@@ -0,0 +1,28 @@
+package shared;
+
+public class Util {
+    /**
+     * Make a string suitable for storage into comma-separated format, by adding
+     * a backslash character before every comma, line-break, and backslash in
+     * the string.
+     *
+     * @param s the original string.
+     * @return the escaped string.
+     */
+    public static String escapeCSVString(String s) {
+        // TODO implement
+        return s;
+    }
+
+    /**
+     * Decode a string encded with {@link #escapeCSVString} back into its
+     * original form.
+     *
+     * @param s the escaped string.
+     * @return the original string.
+     */
+    public static String unescapeCSVString(String s) {
+        // TODO implement
+        return s;
+    }
+}
