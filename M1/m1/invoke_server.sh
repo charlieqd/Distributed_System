3478055a72edb0fae0a42b4cab9b9e8de4861c4b
Jue Liu <vickyjue.liu@mail.utoronto.ca>
2021-02-15 21:13:42 -0500

    add node

---
+++ M1/m1/invoke_server.sh
@@ -0,0 +1,1 @@
+ssh -n $1 nohup java -jar "$ECE419_SERVER_PATH" $2 ERROR &
--- M1/m1/src/app_kvECS/ECSClient.java
+++ M1/m1/src/app_kvECS/ECSClient.java
@@ -1,11 +1,16 @@
 package app_kvECS;
 
-import java.util.Map;
-import java.util.Collection;
-
 import ecs.IECSNode;
+import shared.ServerInfo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 public class ECSClient implements IECSClient {
+
+    ArrayList<ServerInfo> availableToAdd;
 
     @Override
     public boolean start() {
@@ -28,17 +33,49 @@ public class ECSClient implements IECSClient {
     @Override
     public IECSNode addNode(String cacheStrategy, int cacheSize) {
         // TODO
+        if (availableToAdd.isEmpty()) {
+            String msg = "No node is available to add.";
+            logger.error(msg);
+            printError(msg);
+        }
+        Process proc;
+        String script = String
+                .format("invoke_server.sh %s %s", servers.get(0).ip,
+                        servers.get(0).port);
+
+        Runtime run = Runtime.getRuntime();
+        try {
+            proc = run.exec(script);
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error("Failed to add nodes", e);
+            printError("Failed to add node");
+        }
         return null;
     }
 
     @Override
-    public Collection<IECSNode> addNodes(int count, String cacheStrategy, int cacheSize) {
+    public Collection<IECSNode> addNodes(int count, String cacheStrategy,
+                                         int cacheSize) {
         // TODO
+        if (availableToAdd.size() < count) {
+            String msg = String
+                    .format("No enough nodes to add. Number of available node: %s",
+                            availableToAdd.size());
+            logger.error(msg);
+            printError(msg);
+            return null;
+        }
+
+        for (int i = 0; i < count; i++) {
+            addNode(cacheStrategy, cacheSize);
+        }
         return null;
     }
 
     @Override
-    public Collection<IECSNode> setupNodes(int count, String cacheStrategy, int cacheSize) {
+    public Collection<IECSNode> setupNodes(int count, String cacheStrategy,
+                                           int cacheSize) {
         // TODO
         return null;
     }
