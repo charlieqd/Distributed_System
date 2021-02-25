c5e55db3d3e069dd7ba7e8162fdc065696b058ee
Charlie Liu <liuchenhaoqd@gmail.com>
2021-02-24 21:33:13 -0500

    Generate email keys

--- M1/m1/src/benchmarker/PopulateData.java
+++ M1/m1/src/benchmarker/PopulateData.java
@@ -1,8 +1,58 @@
 package benchmarker;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileWriter;
 
 public class PopulateData {
-    public static void main(String[] args) {
+
+    private static ArrayList<String> keys = new ArrayList<>();
+    public static void listFilesForFolder(final File folder) throws
+            IOException {
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                String filePath = fileEntry.getPath();
+                System.out.println(filePath);
+//                System.out.println(filePath.getBytes().length);
+                String fileKey = filePath;
+                if(filePath.length() > 19){
+                    fileKey = filePath.substring(filePath.length() - 19);
+                }
+                keys.add(fileKey);
+                String content = readFile(filePath, StandardCharsets.UTF_8);
+                System.out.println(content);
+            }
+        }
+    }
+
+    static String readFile(String path, Charset encoding)
+            throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
+    public static void main(String[] args) throws IOException {
         // TODO
-        System.out.println("Implement me");
+        int index = 1;
+        String readingFilename = args[0];
+        String keyFileName = args[1];
+        final File folder = new File(readingFilename);
+        listFilesForFolder(folder);
+        System.out.println(keys.size());
+        FileWriter writer = new FileWriter(keyFileName);
+        for(String str: keys) {
+            writer.write(str + System.lineSeparator());
+        }
+        writer.close();
     }
 }
+///Users/liuchenhao/Downloads/perlingiere-d/output.txt
