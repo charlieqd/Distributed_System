8f80e02400a6fa92785c11d1eb3668387ba5a6dc
Charlie Liu <liuchenhaoqd@gmail.com>
2021-03-13 16:53:47 -0500

    test

--- M1/m1/src/server/Replicator.java
+++ M1/m1/src/server/Replicator.java
@@ -2,7 +2,12 @@ package server;
 
 import app_kvServer.KVServer;
 import client.ServerConnection;
+import server.KVStorageDelta.Value;
+import shared.messages.KVMessage;
+import shared.messages.KVMessageImpl;
 
+import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -43,6 +48,23 @@ public class Replicator extends Thread {
 
     private void incrementalReplication(KVStorageDelta delta,
                                         ServerConnection targetConnection) {
+        // shiftshift search files cmd+o: search for class
+        // loop through delta, connection push data
+        // pretend ecs to delete
+        for (Map.Entry<String, Value> entry : delta.getEntrySet()) {
+            String key = entry.getKey();
+            Value value = entry.getValue();
+            int id = -1;
+            try {
+                id = targetConnection.sendRequest(key, value.get(), KVMessage.StatusType.PUT);
+            } catch (IOException e) {
+                targetConnection.disconnect();
+                // throw error put failed
+            }
+            if (id == -1) {
+                // throw error
+            }
+        }
         throw new Error("Not implemented");
     }
 }
