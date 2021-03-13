365a55eb9b1db13cce8ccabe024733d6836885bf
Jue Liu <vickyjue.liu@mail.utoronto.ca>
2021-03-13 17:24:15 -0500

    Add helper function

--- M1/m1/src/server/Replicator.java
+++ M1/m1/src/server/Replicator.java
@@ -9,6 +9,7 @@ import shared.messages.KVMessageImpl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -65,18 +66,87 @@ public class Replicator extends Thread {
                                  String rangeEnd,
                                  ServerConnection targetConnection) {
         try {
-            int request_id = targetConnection
+            int requestId = targetConnection
                     .sendRequest(new KVMessageImpl(null, null,
                             null, KVMessage.StatusType.ECS_DELETE_DATA,
                             new MoveDataArgs(rangeStart, rangeEnd, null, 0)));
 
+            if (requestId == -1) {
+                logger.error("Unable to delete replicate data");
+                return false;
+            }
+
+            try {
+                KVMessage resMessage = targetConnection
+                        .receiveMessage(requestId);
+                KVMessage.StatusType resStatus = resMessage.getStatus();
+                if (resStatus == KVMessage.StatusType.ECS_SUCCESS) {
+                    return true;
+                } else {
+                    logger.error(
+                            "Failed to send data to next server: response status = " + resStatus
+                                    .toString());
+                    return false;
+                }
+            } catch (Exception e) {
+                logger.error(
+                        "Failed to receive ecs delete response from target server");
+                return false;
+            }
         } catch (Exception e) {
-            logger.error("Unable to delete transferred data");
+            logger.error("Unable to delete replicate data");
+            return false;
         }
+        deleteReplicateData(rangeStart, rangeEnd, targetConnection);
     }
 
     private void incrementalReplication(KVStorageDelta delta,
                                         ServerConnection targetConnection) {
         throw new Error("Not implemented");
+    }
+
+    private boolean sendCommandToNode(ServerConnection targetConnection,
+                                      KVMessage msg,
+                                      Set<KVMessage.StatusType> successStatus) {
+        try {
+            int requestId = targetConnection
+                    .sendRequest(msg);
+
+            if (requestId == -1) {
+                logger.error(
+                        String.format("Unable to send message to node %s:%d",
+                                targetConnection.getAddress(),
+                                targetConnection.getPort()));
+                return false;
+            }
+
+            try {
+                KVMessage resMessage = targetConnection
+                        .receiveMessage(requestId);
+                KVMessage.StatusType resStatus = resMessage.getStatus();
+                if (successStatus.contains(resStatus)) {
+                    return true;
+                } else {
+                    logger.error(String.format(
+                            "Node %s:%d responded with failure: " + resMessage
+                                    .getValue(),
+                            targetConnection.getAddress(),
+                            targetConnection.getPort()));
+                    return false;
+                }
+            } catch (Exception e) {
+                logger.error(String.format(
+                        "Failed to receive response from Node %s:%d",
+                        targetConnection.getAddress(),
+                        targetConnection.getPort()));
+                return false;
+            }
+        } catch (Exception e) {
+            logger.error(String.format(
+                    "Failed to send command to Node %s:%d",
+                    targetConnection.getAddress(),
+                    targetConnection.getPort()));
+            return false;
+        }
     }
 }
