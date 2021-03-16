35aa1cc167cc3e61180ce0ac4c80b0c9f1a34130
Charlie Liu <liuchenhaoqd@gmail.com>
2021-03-16 01:20:13 -0400

    add test

--- M1/m1/src/testing/AdditionalTest.java
+++ M1/m1/src/testing/AdditionalTest.java
@@ -26,6 +26,7 @@ import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotSame;
 
 public class AdditionalTest {
 
@@ -392,16 +393,16 @@ public class AdditionalTest {
     }
 
     @Test
-    public void testKVServerReplica() throws Exception {
+    public void testReplicaRead() throws Exception {
         KVServer server = null;
         ServerConnection connection = null;
         KVServer server1 = null;
         ServerConnection connection1 = null;
         try {
             String rootPath = folder.newFolder().toString();
-            server = new KVServer(
-                    new KVStorage(rootPath, new MD5PrefixKeyHashStrategy(1),
-                            1024, IKVServer.CacheStrategy.LRU), new Protocol(),
+            KVStorage storage = new KVStorage(rootPath, new MD5PrefixKeyHashStrategy(1),
+                    1024, IKVServer.CacheStrategy.LRU);
+            server = new KVServer(storage, new Protocol(),
                     new KVMessageSerializer(), 50001, "testServer", null);
             server.start();
             server.startServing();
@@ -412,9 +413,9 @@ public class AdditionalTest {
                             "92eb5ffee6ae2fec3ad71c777531578f"))));
             Thread.sleep(1000);
 
+            KVStorage storage1 = new KVStorage(rootPath, new MD5PrefixKeyHashStrategy(1), 1024, IKVServer.CacheStrategy.LRU);
             server1 = new KVServer(
-                    new KVStorage(rootPath, new MD5PrefixKeyHashStrategy(1),
-                            1024, IKVServer.CacheStrategy.LRU), new Protocol(),
+                    storage1, new Protocol(),
                     new KVMessageSerializer(), 50002, "testServer2", null);
             server1.start();
             server1.startServing();
@@ -425,27 +426,97 @@ public class AdditionalTest {
                             "92eb5ffee6ae2fec3ad71c777531578f"))));
             Thread.sleep(1000);
 
-            connection1 = new ServerConnection(new Protocol(),
+            connection = new ServerConnection(new Protocol(),
                     new KVMessageSerializer(), "127.0.0.1", 50001);
+            connection.connect();
+            connection1 = new ServerConnection(new Protocol(),
+                    new KVMessageSerializer(), "127.0.0.1", 50002);
             connection1.connect();
 
             int id = connection.sendRequest("a", null,
                     KVMessage.StatusType.GET);
             KVMessage message = connection.receiveMessage(id);
-            assertNotEquals(KVMessage.StatusType.NOT_RESPONSIBLE,
-                    message.getStatus());
-
-            id = connection.sendRequest("b", null,
-                    KVMessage.StatusType.GET);
-            message = connection.receiveMessage(id);
             assertEquals(KVMessage.StatusType.GET_ERROR, message.getStatus());
+            assertNotSame(KVMessage.StatusType.NOT_RESPONSIBLE, message.getStatus());
+        } finally {
+            if (connection != null) {
+                connection.disconnect(true);
+            }
+            if (connection1 != null) {
+                connection1.disconnect(true);
+            }
+            if (server != null) {
+                server.shutDown();
+            }
+            if (server1 != null) {
+                server1.shutDown();
+            }
+        }
+    }
+
+    @Test
+    public void testReplicaWrite() throws Exception {
+        KVServer server = null;
+        ServerConnection connection = null;
+        KVServer server1 = null;
+        ServerConnection connection1 = null;
+        try {
+            String rootPath = folder.newFolder().toString();
+            KVStorage storage = new KVStorage(rootPath, new MD5PrefixKeyHashStrategy(1),
+                    1024, IKVServer.CacheStrategy.LRU);
+            server = new KVServer(storage, new Protocol(),
+                    new KVMessageSerializer(), 50001, "testServer", null);
+            server.start();
+            server.startServing();
+            server.updateMetadata(new Metadata(Arrays.asList(
+                    new ECSNode("testServer2", "127.0.0.1", 50002,
+                            "0cc175b9c0f1b6a831c399e269772661"),
+                    new ECSNode("testServer", "127.0.0.1", 50001,
+                            "92eb5ffee6ae2fec3ad71c777531578f"))));
+            Thread.sleep(1000);
+
+            KVStorage storage1 = new KVStorage(rootPath, new MD5PrefixKeyHashStrategy(1), 1024, IKVServer.CacheStrategy.LRU);
+            server1 = new KVServer(
+                    storage1, new Protocol(),
+                    new KVMessageSerializer(), 50002, "testServer2", null);
+            server1.start();
+            server1.startServing();
+            server1.updateMetadata(new Metadata(Arrays.asList(
+                    new ECSNode("testServer2", "127.0.0.1", 50002,
+                            "0cc175b9c0f1b6a831c399e269772661"),
+                    new ECSNode("testServer", "127.0.0.1", 50001,
+                            "92eb5ffee6ae2fec3ad71c777531578f"))));
+            Thread.sleep(1000);
+
+            connection = new ServerConnection(new Protocol(),
+                    new KVMessageSerializer(), "127.0.0.1", 50001);
+            connection.connect();
+            connection1 = new ServerConnection(new Protocol(),
+                    new KVMessageSerializer(), "127.0.0.1", 50002);
+            connection1.connect();
+
+            int id = connection.sendRequest("a", "1",
+                    KVMessage.StatusType.PUT);
+            KVMessage message = connection.receiveMessage(id);
+            assertEquals(KVMessage.StatusType.NOT_RESPONSIBLE, message.getStatus());
+
+            id = connection1.sendRequest("a", "1",
+                    KVMessage.StatusType.PUT);
+            message = connection1.receiveMessage(id);
+            assertEquals(KVMessage.StatusType.PUT_SUCCESS, message.getStatus());
 
         } finally {
             if (connection != null) {
                 connection.disconnect(true);
             }
+            if (connection1 != null) {
+                connection1.disconnect(true);
+            }
             if (server != null) {
                 server.shutDown();
+            }
+            if (server1 != null) {
+                server1.shutDown();
             }
         }
     }
